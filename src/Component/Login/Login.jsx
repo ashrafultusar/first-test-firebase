9e2f87537ff3df04f4e234331a8b8505e22d3e50
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-03-31 16:30:49 +0600

    added update

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.init';
 import { GoogleAuthProvider } from "firebase/auth";
 
 
 const Login = () => {
+    const [users , setUsers]=useState(null)
+
     const auth = getAuth(app);
-    const [users , setUsers]=useState('')
+    
     const provider = new GoogleAuthProvider();
 
 
@@ -22,10 +24,36 @@ const Login = () => {
             })
 }
 
+    
+    const handelSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result)
+                setUsers(null)
+            })
+            .catch(error => {
+            console.log(error)
+        })
+    }
+    
     return (
         <div>
+            {
+                users?
+            <button onClick={handelSignOut}>Sign Out</button>:
             <button onClick={handelGoogleSignIn}>Google login</button>
-       
+}
+
+
+
+            {
+                users&&<div>
+                <h2>User: {users.displayName }</h2>
+                    <h3>Email: {users.email }</h3>
+                    <h3><img src={users.photoURL } alt="" /></h3>
+                
+                </div>
+            }
         </div>
     );
 };
